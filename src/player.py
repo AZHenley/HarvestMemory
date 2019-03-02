c781cc5c95cb3294ffa03da8aafeb6201a695134
Austin Henley <azh321@gmail.com>
2019-03-02 15:54:37 -0500

    Added name.

--- src/player.py
+++ src/player.py
@@ -1,11 +1,13 @@
 class Player(object):
     fruit = 0
     delay = 0
+    name = "knoxville"
     instructions = []
     labels = {}
     next = 0 # Next instruction to be executed.
     registers = {'r0': 0, 'r1': 0, 'r2': 0, 'r3': 0}
 
-    def __init__(self, instructions=None, labels=None):
+    def __init__(self, name=None, instructions=None, labels=None):
+        self.name = name
         self.instructions = instructions
         self.labels = labels
